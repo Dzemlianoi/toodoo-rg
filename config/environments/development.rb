7d81152741c40f75c341d7101fda7051d4c67699
Denis Zemlianoi <dzemlianoi@gmail.com>
2017-04-02 18:26:59 +0300

    ref

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  config.debug_exception_response_format = :default
   config.cache_classes = false
   config.eager_load = false
   config.consider_all_requests_local = true
