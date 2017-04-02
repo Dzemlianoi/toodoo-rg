e1966b94a1e9899d0defc05bad95b8c942cbf080
Denis Zemlianoi <dzemlianoi@gmail.com>
2017-04-02 20:20:06 +0300

    comments added

---
+++ db/migrate/20170402165951_create_comments.rb
@@ -0,0 +1,10 @@
+class CreateComments < ActiveRecord::Migration[5.0]
+  def change
+    create_table :comments do |t|
+      t.string :comment_text
+      t.timestamps
+    end
+    add_reference :comments, :task, index: true, foreign_key: true
+    add_reference :comments, :user, index: true, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170402162054) do
+ActiveRecord::Schema.define(version: 20170402165951) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "comment_text"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.integer  "task_id"
+    t.integer  "user_id"
+    t.index ["task_id"], name: "index_comments_on_task_id", using: :btree
+    t.index ["user_id"], name: "index_comments_on_user_id", using: :btree
+  end
 
   create_table "projects", force: :cascade do |t|
     t.string   "title"
@@ -55,6 +65,8 @@ ActiveRecord::Schema.define(version: 20170402162054) do
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true, using: :btree
   end
 
+  add_foreign_key "comments", "tasks"
+  add_foreign_key "comments", "users"
   add_foreign_key "projects", "users"
   add_foreign_key "tasks", "projects"
 end
