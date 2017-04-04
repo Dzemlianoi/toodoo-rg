fe3f4a73320a08f36eca596f75f5eab660e7e9da
Denis Zemlianoi <dzemlianoi@gmail.com>
2017-04-04 11:22:06 +0300

    cancan rescueing and responding just on :json

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
   include CanCan::ControllerAdditions
+
+  respond_to :json
+
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_url, :alert => exception.message
+  end
 end
