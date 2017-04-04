9863776f3cbd14b56bf4ffa49c24e9673c72dc0b
Denis Zemlianoi <dzemlianoi@gmail.com>
2017-04-04 11:26:01 +0300

    routes redone

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
-  root 'tasks#index'
-  resources :projects do
-    resources :tasks
-  end
+  root 'projects#index'
+  resources :projects
   resources :tasks
+  resources :comments
 end
