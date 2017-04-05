22a0ee99e171a7577eae53effd5bb2877c6196a8
Denis Zemlianoi <dzemlianoi@gmail.com>
2017-04-05 15:17:56 +0300

    model specs added

---
+++ spec/models/task_spec.rb
@@ -0,0 +1,48 @@
+RSpec.describe Task, type: :model do
+  context 'fields' do
+    subject { create :task, project: create(:project) }
+
+    it { is_expected.to belong_to(:project) }
+    it { is_expected.to have_many(:comments).dependent(:destroy) }
+    it { is_expected.to validate_presence_of(:title) }
+    it { is_expected.to validate_presence_of(:priorite) }
+    it { is_expected.to validate_presence_of(:project) }
+    it { is_expected.to validate_length_of(:title).is_at_least 2 }
+    it { is_expected.to validate_length_of(:title).is_at_most 40 }
+    it { is_expected.to validate_numericality_of(:priorite).only_integer }
+    it { is_expected.to validate_numericality_of(:priorite).is_greater_than 0 }
+  end
+
+  context 'priorite setting' do
+    let(:project) { create :project }
+    let!(:task_one) { create :task, project: project }
+    binding.pry
+    let!(:task_two) { create :task, project: project }
+    let!(:task_three) { create :task, project: project }
+
+    it 'priorite should add 1 after each task creation' do
+      expect(task_one.priorite).to be_equal(1)
+      expect(task_two.priorite).to be_equal(2)
+    end
+
+    it 'should change all priorites which are lower than it' do
+      task.destroy
+      expect(task_two.priorite).to be_equal(1)
+      expect(task_three.priorite).to be_equal(2)
+    end
+  end
+
+  context 'deadline' do
+    it 'correct' do
+      task = build :with_invalid_deadline
+      task.valid?
+      expect(task.errors[:deadline].first).to eq(I18n.t('errors.task.deadline'))
+    end
+
+    it 'incorrect' do
+      task = build :with_valid_deadline
+      task.valid?
+      expect(task.errors[:deadline]).to be_empty
+    end
+  end
+end
