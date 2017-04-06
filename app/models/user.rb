048ca4565aa9ce53c0ef75a870b4a48e1368bfa0
Denis Zemlianoi <dzemlianoi@gmail.com>
2017-04-06 11:30:57 +0300

    unneccessery has_many comments relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,5 @@ class User < ActiveRecord::Base
   include DeviseTokenAuth::Concerns::User
 
   has_many :projects, dependent: :destroy
-  has_many :comments, dependent: :destroy
-
   validates_presence_of :email, :encrypted_password
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,6 @@
 RSpec.describe User, type: :model do
   context 'fields' do
     it { is_expected.to have_many(:projects).dependent(:destroy) }
-    it { is_expected.to have_many(:comments).dependent(:destroy) }
     it { is_expected.to validate_presence_of(:email) }
     it { is_expected.to validate_presence_of(:encrypted_password) }
   end
