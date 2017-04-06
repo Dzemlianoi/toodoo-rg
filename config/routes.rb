c5f0589e6ebaf82529c4df30e1b7ab42d64c951b
Denis Zemlianoi <dzemlianoi@gmail.com>
2017-04-06 13:59:29 +0300

    routes redone tmp because of auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
-  root 'projects#index'
+  root 'tasks#index'
   resources :projects
   resources :tasks
   resources :comments
