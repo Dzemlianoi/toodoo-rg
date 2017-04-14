f2b5d46ce3972da3fbff0423686541579352ba12
Denis Zemlianoi <dzemlianoi@gmail.com>
2017-04-14 14:29:42 +0300

    load through current_user

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,15 +1,13 @@
 # frozen_string_literal: true
 
 class ProjectsController < ApplicationController
-  load_and_authorize_resource
+  load_and_authorize_resource through: :current_user
 
   def index
     render json: @projects
   end
 
   def create
-    # fix later
-    @project.user_id = 1
     render json: @project and return if @project.save
     render json: @project.errors, status: :unprocessable_entity
   end
