52dfdd8a90a44dd9c11341c33e4e8274dc8fc2e6
Denis Zemlianoi <dzemlianoi@gmail.com>
2017-04-16 07:17:51 +0300

    order_up and order_down actions implemented

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
   root 'tasks#index'
   resources :projects
-  resources :tasks
+  resources :tasks do
+    member do
+      patch 'order_up'
+      patch 'order_down'
+    end
+  end
   resources :comments
 end
