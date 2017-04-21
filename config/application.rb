bf3f91bc78d9a0e5e3e47e497569c61852369937
Denis Zemlianoi <dzemlianoi@gmail.com>
2017-04-21 12:49:34 +0300

    middleware for session

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,9 @@ Bundler.require(*Rails.groups)
 module Toodoo
   class Application < Rails::Application
     config.api_only = true
+    config.middleware.use ActionDispatch::Flash
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
     config.assets.initialize_on_precompile = false
     config.middleware.use Rack::Cors do
       allow do
